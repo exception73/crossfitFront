336658f930d8a2e1565102a253d0d438586aa4c8
Gautam Khatri <109818831+exception73@users.noreply.github.com>
2023-05-24 22:21:06 +0530

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,4 @@
 // const baseURL = "http://localhost:3001/api/v1";
-const baseURL = "https://backendcrossfit.onrender.com"
+const baseURL = "https://crossfitbackend.onrender.com"
+
 export default baseURL;
