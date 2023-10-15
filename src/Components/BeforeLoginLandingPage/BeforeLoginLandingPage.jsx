2db2a620913373c3888c37c478411aeffd1624bb
Gautam Khatri <109818831+exception73@users.noreply.github.com>
2023-10-15 14:52:32 +0530

    Update BeforeLoginLandingPage.jsx

--- src/Components/BeforeLoginLandingPage/BeforeLoginLandingPage.jsx
+++ src/Components/BeforeLoginLandingPage/BeforeLoginLandingPage.jsx
@@ -6,7 +6,8 @@ const BeforeLoginLandingPage = () => {
   return (
     <div  className='before-login-landing-page'>
         <img className='before-login-landing-page-crossfit-img' src="https://res.cloudinary.com/dycitvrpg/image/upload/v1681382796/logo_xx6npu.png" alt="crossfit" />
-        <div className='before-login-landing-page-btn-div'>
+       <img src="https://indianmemetemplates.com/wp-content/uploads/dhat-teri-maa-ki.jpg" alt="naughty hora bhen k lode" />
+      <div className='before-login-landing-page-btn-div'>
             <Link className='before-login-landing-page-signup' to='/signup'>Sign Up</Link>
             <Link className='before-login-landing-page-signin' to='/signin'>Sign In</Link>
         </div>
